7929a65d587bf72b51f4cb719885aa0612f8f383
vicberg <40573356+vicberg@users.noreply.github.com>
2019-07-28 09:32:34 -0600

    1.1.4 Beta

--- combattracker.js
+++ combattracker.js
@@ -1,5 +1,5 @@
 /* 
- * Version 1.1.3 Beta
+ * Version 1.1.43 Beta
  * Made By Robin Kuiper
  * Changes in Version 0.2.1 by The Aaron
  * Changes in Version 0.3.0 and greater by Victor B
@@ -17,7 +17,7 @@ var CombatTracker = CombatTracker || (function() {
     'use strict';
 
     let round = 1,
-	    version = '1.1.3 Beta',
+	    version = '1.1.4 Beta',
         timerObj,
         intervalHandle,
         debug = true,
@@ -914,7 +914,7 @@ var CombatTracker = CombatTracker || (function() {
         removeButton  = makeImageButton('!ct remove',deleteImage,'Remove Condition','transparent',18)
         
         if (state[combatState].conditions) {
-            state[combatState].conditions.forEach(condition => {
+            [... state[combatState].conditions].forEach(condition => {
                 
                 if (condition.id ==  token.get("_id")) {
                     output  +=  '<div>'
